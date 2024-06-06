206561c064ea4476b3334416d38adb77e611d40d
wessam18 <wessamabdelnabi903@gmail.com>
2024-06-07 00:06:44 +0300

    5,6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""import module"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Return the sum of two floats"""
+    return sum(mxd_lst)
