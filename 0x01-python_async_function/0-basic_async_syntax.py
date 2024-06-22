21566a55ebdd609d6b5ff177217388f0545cb188
wessam18 <wessamabdelnabi903@gmail.com>
2024-06-22 10:03:14 +0300

    0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" """
+"""import module"""
 
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """ """
-    delay = random.randrange() * max_delay
+    """coroutine code """
+    delay = random.random() * max_delay
     await asyncio.sleep(delay)
     return delay
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
-""" Module documentation """
+"""import module"""
 
 import asyncio
+import random
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """async function"""
+async def wait_n(n: int, max_delay: int):
     tasks = [wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
-    return sorted(delays)
+    
+    delays = []
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    
+    return delays
