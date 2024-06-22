0ccaf149f7be36410ae036f574513c848698a093
wessam18 <wessamabdelnabi903@gmail.com>
2024-06-22 10:05:38 +0300

    0

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,6 +8,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int):
+    """spawn coroutine"""
     tasks = [wait_random(max_delay) for _ in range(n)]
 
     delays = []
