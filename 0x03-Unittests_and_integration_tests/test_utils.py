e30c2b3b5b5a515956f1998aeca5e19d8e5725f0
wessam18 <wessamabdelnabi903@gmail.com>
2024-07-02 09:03:56 +0300

    .

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,8 +2,11 @@
 """import module"""
 
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 access_nested_map = __import__('utils').access_nested_map
+get_json = __import__('utils').get_json
+
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -26,3 +29,23 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), repr(path[-1]))
+
+
+class TestGetJson(unittest.TestCase):
+    """ """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, expected):
+        """ """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        expected.return_value = mock_response
+        
+        # Call the function with the test URL
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
+        # Assert that the requests.get method was called with the correct URL
+        expected.assert_called_once_with(test_url)
